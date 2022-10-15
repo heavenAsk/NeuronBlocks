d5e7ea96869aa3cf2dc4d4437c9749dfe7a69bc1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 08:10:46 +0000

    Adding tarfile member sanitization to extractall()

--- dataset/get_20_newsgroups.py
+++ dataset/get_20_newsgroups.py
@@ -31,7 +31,26 @@ class NewsGroup(object):
                 urldownload.urlretrieve(self.url, file_path)
             with tarfile.open(file_path, 'r', encoding='utf-8') as fin:
                 print('Extracting...')
-                fin.extractall(self.params.root_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(fin, self.params.root_dir)
         return path
 
     def read_process_file(self, file_path):
